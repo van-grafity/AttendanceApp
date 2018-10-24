ffd075a1bb51a5dabae2a4af71f8c0a14e4ec6b9
van-grafity <vanlaserblack@gmail.com>
2018-10-24 17:55:03 +0700

    (Unit Test) Attendance Model and Preparation Case

--- app/src/androidTest/java/id/co/ipos/hadir/attlogger/Logic_Case_InstrumentedTest.java
+++ app/src/androidTest/java/id/co/ipos/hadir/attlogger/Logic_Case_InstrumentedTest.java
@@ -2,6 +2,8 @@ package id.co.ipos.hadir.attlogger;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
@@ -10,7 +12,12 @@ import org.junit.runner.RunWith;
 
 import id.co.ipos.hadir.attlogger.Activity.AttendanceActivity;
 
-import static org.junit.Assert.assertEquals;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,18 +26,23 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class Logic_Case_InstrumentedTest{
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("id.co.ipos.hadir.attlogger", appContext.getPackageName());
-    }
 
     @Rule
     public ActivityTestRule<AttendanceActivity> activityTestRule
             = new ActivityTestRule<>(AttendanceActivity.class);
 
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        onView((withId(R.id.button_main))).perform(ViewActions.click());
+        onView((withId(R.id.username))).perform(typeText("test_user"));
+        onView((withId(R.id.password))).perform(typeText("test_password"));
+        onView((withId(R.id.layout))).check(matches(isDisplayed()));
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("id.co.ipos.hadir.attlogger", appContext.getPackageName());
+    }
 
 
 }
---
+++ app/src/test/java/id/co/ipos/hadir/attlogger/Attendance_view_UnitTest.java
@@ -0,0 +1,14 @@
+package id.co.ipos.hadir.attlogger;
+
+public class Attendance_view_UnitTest {
+
+    /*@Test
+    public void test_show_Ready_State() {
+        AttendanceViewImpl attendanceView = new AttendanceViewImpl();
+        attendanceView.showReadyState();
+    }
+
+    @Rule
+    public ActivityTestRule<AttendanceActivity> activityTestRule
+            = new ActivityTestRule<>(AttendanceActivity.class);*/
+}
